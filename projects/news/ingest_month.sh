28dc0e8bc75c5aa04338bcb3a0ad3296254aff08
Kevin Greenan <kmgreen2@gmail.com>
2022-04-30 08:23:22 -0700

    Bucket/region should be a param

--- projects/news/ingest_month.sh
+++ projects/news/ingest_month.sh
@@ -6,13 +6,14 @@ YEAR=${1?Year in YYYY format}
 MONTH=${2?Month in MM format}
 START_OFFSET=${3?Start offset in the index file}
 LENGTH=${4?Number of index files to process}
-BUCKET_NAME=kmg-news-archive
+# Format 'region.bucket'
+REGION_BUCKET_NAME=${5}
 
 ${ROOTDIR}/projects/news/fetch_news_index_entries.sh ${ROOTDIR} ${YEAR} ${MONTH} ${START_OFFSET} ${LENGTH} || exit 1
 
-if [[ -n ${BUCKET_NAME} ]]; then
+if [[ -n ${REGION_BUCKET_NAME} ]]; then
     pipenv run python3 ${ROOTDIR}/src/main.py -i ${ROOTDIR}/news-${MONTH}-${YEAR}-${START_OFFSET}-${LENGTH}.files \
-    -o s3://${BUCKET_NAME}/${YEAR}-${MONTH}-${START_OFFSET}-${LENGTH}.json -p news  -I warc-index -t 32
+    -o s3://${REGION_BUCKET_NAME}/${YEAR}-${MONTH}-${START_OFFSET}-${LENGTH}.json -p news  -I warc-index -t 32
 else
     pipenv run python3 ${ROOTDIR}/src/main.py -i ${ROOTDIR}/news-${MONTH}-${YEAR}-${START_OFFSET}-${LENGTH}.files \
     -o file:///tmp/crawlthenews-${YEAR}-${MONTH}-${START_OFFSET}-${LENGTH}.json -p news  -I warc-index -t 32
